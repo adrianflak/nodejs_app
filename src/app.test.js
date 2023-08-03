1c9af991e6221af0241dbdc2f2588f31d40d9c57
Ubuntu <adrian@vm1.u3rz534lth1ethqar2mmbknije.ax.internal.cloudapp.net>
2023-08-03 17:32:39 +0000

    first commit

---
+++ src/app.test.js
@@ -0,0 +1,14 @@
+const app = require("./app")
+const supertest = require("supertest")
+const request = supertest(app)
+
+describe("/test endpoint", () => {
+	it("Aplikacja zwróci odpowiedź", async() => {
+	const response = await request.get("/test")
+	expect(response.status).toBe(200)
+	expect(response.text).toBe("Hello World")
+
+})
+})
+
+
